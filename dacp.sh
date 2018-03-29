00ab383df77f8b91327791532508a8e9b62c6cd3
Martin Shaw <15090190@stu.mmu.ac.uk>
2018-03-29 21:34:44 +0100

    Testing DACP script...

--- dacp.sh
+++ dacp.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# (D)atabase backup, then Git (A)dd, Git (C)ommit, Git (P)ush script.  Created by Martin Shaw.
+
 sudo mysqldump -uroot -ptoor geochat > databasebackup.sql;
 git add --all;
 git commit -m "$1";
 git push -u origin master;
 
-echo "DONE!";
+echo "\n\nDONE!\n";
